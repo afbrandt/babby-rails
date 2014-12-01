8e52db12d3a6c537079a8c7052a1605cc01e2229
Andrew Brandt <abrandt@makeschool.com>
2014-12-01 13:57:25 -0800

    finish chapter 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   def hello
-    render text: "hello, world!"
+    render text: "¡hola, mundo!"
   end
+
+  def goodbye
+    render text: "goodbye, world"
+  end
+
 end
